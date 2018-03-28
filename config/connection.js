6df9fbb8f73205b5134f4fb5332f3e41b26ee37d
jens87alpen <jenshoffmann20@gmail.com>
2018-03-28 13:28:17 -0700

    jawsDB

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+
+var connection;
+
+
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "jensMy87",
+        database: "blogger",
+    });
+};
+
+module.exports = connection;
